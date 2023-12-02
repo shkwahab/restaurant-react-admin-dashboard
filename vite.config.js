408e5dda031d2011f5c881ea17960e335ef863db
shkwahab <wahab209266@gmail.com>
2023-12-02 10:54:23 +0500

    order page mobile responsiveness

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     port:3000
-//   }
-// })
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build:{
-  //   outDir: "build"
+  // server:{
+  //   port:3000
   // }
 })
